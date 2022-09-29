d0042e13711c88e473ba809c5a7965c0b5461ef5
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-09-29 21:26:13 +0300

    Added First Route

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+app.get("/",(req,res) => {
+    res.send("Hello world");
+})
+const PORT = process.env.PORT || 5100;
+app.listen(PORT,() => {
+    console.log(`Server runing at : ${PORT}`);
+})
