b02bbef2001163c25f38c825893c81fc0ddb671b
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-09-29 21:35:16 +0300

    TestHeroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/",(req,res) => {
-    res.send("Hello world");
+    res.send("Hello");
 })
 const PORT = process.env.PORT || 5100;
 app.listen(PORT,() => {
