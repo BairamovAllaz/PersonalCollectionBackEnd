9f661781bae1d0c900d9b057f6a9710aa64d2335
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-10-06 00:20:41 +0300

    bo

--- app.js
+++ app.js
@@ -8,7 +8,30 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 //MIDDLEWARES
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: ["http://localhost:3000"]
+    })
+);
+app.all("/*", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+app.set("trust proxy", 1); // trust first proxy
+app.use(
+    session({
+        secret: "836wer89r",
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            secure: true,
+            sameSite: "none",
+        },
+    })
+);
+
 app.use("/uploads",require("express").static("uploads"));
 app.use(cookieParser("secretcode"));
 app.use(
