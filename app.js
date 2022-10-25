53ff9276bd7fa657d93c8c119a844bcd73cb98f0
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-10-26 01:17:35 +0300

    kj

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 //MIDDLEWARES
 app.all("/*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://main--sprightly-boba-3c9eab.netlify.app"
+  );
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
