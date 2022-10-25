bf9998cfa453ed0a3669a0a51bdb066be0f499ad
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-10-26 01:15:35 +0300

    k

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 //MIDDLEWARES
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.use(
   cors({
     credentials: true,
