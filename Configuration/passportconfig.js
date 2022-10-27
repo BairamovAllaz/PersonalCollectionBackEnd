4bd8bc03580e3ef309351d36c257e46a3f07e0d4
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-10-27 09:00:20 +0300

    Init google

--- Configuration/passportconfig.js
+++ Configuration/passportconfig.js
@@ -25,31 +25,31 @@ module.exports = function (passport) {
       }
     )
   );
-  // passport.use(
-  //   new GoogleStrategy(
-  //     {
-  //       clientID: process.env.GOOGLEAPIKEYCLIENTID,
-  //       clientSecret: process.env.GOOGLEAPIKEYCLIENTSECRET,
-  //       callbackURL: "http://localhost:5100/v1/api/sessions/google",
-  //       proxy: true,
-  //     },
-  //     async (accessToken, refreshToken, profile, done) => {
-  //       const newuser = {
-  //         firstName: profile.name.givenName,
-  //         lastName: profile.name.familyName,
-  //         email: profile.emails[0].value,
-  //         image: profile.photos[0].value,
-  //         password: crypto.randomUUID(),
-  //         userRole: 0,
-  //         authType: "google",
-  //         updatedAt: new Date(),
-  //         createdAt: new Date(),
-  //       };
-  //       const res = await AuthService.CreateIfDontExsist(newuser);
-  //       done(false, res);
-  //     }
-  //   )
-  // );
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLEAPIKEYCLIENTID,
+        clientSecret: process.env.GOOGLEAPIKEYCLIENTSECRET,
+        callbackURL: "http://localhost:5100/v1/api/sessions/google",
+        proxy: true,
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const newuser = {
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          email: profile.emails[0].value,
+          image: profile.photos[0].value,
+          password: crypto.randomUUID(),
+          userRole: 0,
+          authType: "google",
+          updatedAt: new Date(),
+          createdAt: new Date(),
+        };
+        const res = await AuthService.CreateIfDontExsist(newuser);
+        done(false, res);
+      }
+    )
+  );
   passport.serializeUser((user, cb) => {
     cb(null, user.Id);
   });
