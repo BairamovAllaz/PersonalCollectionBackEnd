2a8622de86fda1773f5084fa9d40996a066f950a
BairamovAllaz <ellezbayramov2004@gmail.com>
2023-07-27 12:45:37 +0400

    Fixed database connection

--- Configuration/connection.js
+++ Configuration/connection.js
@@ -1,9 +1,10 @@
 const {Sequelize} = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config();
-const database = new Sequelize(process.env.DATABASE,process.env.USER, process.env.DATABASE_PASSWORD,
-    {host: process.env.HOST, dialect: "mysql"}
-);
+const database = new Sequelize("personalCollection", "root", "ellez2004", {
+  host: "localhost",
+  dialect: "mysql",
+});
 module.exports = database;
 database.authenticate().then(() => {
     console.log("Database connected");
